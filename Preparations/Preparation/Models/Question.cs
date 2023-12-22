98c63f9e5402d308aad119918a8c3cafc5d57d72
Alexander <sany199574@gmail.com>
2023-12-22 15:16:05 +0300

    Добавлена модель Question

---
+++ Preparations/Preparation/Models/Question.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace Preparation.Models
+{
+    public class Question
+    {
+        Guid Id { get; set; }
+        [DisplayName("Вопросс")]
+        string Name { get; set; } = string.Empty;
+
+        [DisplayName("Ответ")]
+        string Answer { get; set; } = string.Empty;
+
+        [DisplayName("Тема")]
+        string Subject { get; set; } = string.Empty;
+    }
+}
