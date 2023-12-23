944dfe11cce38e16cc1e50028f63e8dec4218122
Alexander <sany199574@gmail.com>
2023-12-23 12:00:59 +0300

    Добавлен ApplicationContext

---
+++ Preparations/Preparation/Data/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Preparation.Models;
+
+namespace Preparation.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Subject> Subjects { get; set; } = null!;
+        public DbSet<Question> Questions { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
