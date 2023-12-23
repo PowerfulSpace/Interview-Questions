63ef699f9adc2b8f34f7a71d3e00425d3ead8840
Alexander <sany199574@gmail.com>
2023-12-23 07:15:07 +0300

    Добавил модель,  Subject

--- Preparations/Preparation/Models/Question.cs
+++ Preparations/Preparation/Models/Question.cs
@@ -5,13 +5,16 @@ namespace Preparation.Models
     public class Question
     {
         Guid Id { get; set; }
+
         [DisplayName("Вопросс")]
         string Name { get; set; } = string.Empty;
 
         [DisplayName("Ответ")]
         string Answer { get; set; } = string.Empty;
 
-        [DisplayName("Тема")]
-        string Subject { get; set; } = string.Empty;
+
+
+        Guid SubjectId { get; set; }
+        Subject Subject { get; set; }
     }
 }
---
+++ Preparations/Preparation/Models/Subject.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace Preparation.Models
+{
+    public class Subject
+    {
+        Guid Id { get; set; }
+        List<Question> Questions { get; set; }
+    }
+}
