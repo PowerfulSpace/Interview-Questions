6c76932d9e0ccfb0bf04e1cb92f4f231f7bf62d6
Alexander <sany199574@gmail.com>
2023-12-23 12:44:34 +0300

    Добавлен контроллер :  - QuestionController

--- Preparations/Preparation/Controllers/QuestionController.cs
+++ Preparations/Preparation/Controllers/QuestionController.cs
@@ -1,12 +1,107 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Preparation.Interfaces;
+using Preparation.Models;
 
 namespace Preparation.Controllers
 {
     public class QuestionController : Controller
     {
-        public IActionResult Index()
+        private readonly IQuestion _questionRepository;
+        public QuestionController(IQuestion questionRepository)
         {
-            return View();
+            _questionRepository = questionRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var items = await _questionRepository.GetItemsAsync();
+
+            return View(items);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var item = new Question();
+
+            return View(item);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Question question)
+        {
+            if (ModelState.IsValid)
+            {
+                await _questionRepository.GreateAsync(question);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(question);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var item = await _questionRepository.GetItemAsync(id);
+
+            if (item != null)
+            {
+                return View(item);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Question question)
+        {
+            if (ModelState.IsValid)
+            {
+                await _questionRepository.EditAsync(question);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(question);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var item = await _questionRepository.GetItemAsync(id);
+
+            if (item != null)
+            {
+                return View(item);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Question question)
+        {
+            if (ModelState.IsValid)
+            {
+                await _questionRepository.DeleteAsync(question);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(question);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var item = await _questionRepository.GetItemAsync(id);
+
+            if (item != null)
+            {
+                return View(item);
+            }
+
+            return NotFound();
         }
     }
 }
